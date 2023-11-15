1647c160b224ce7df4f0a9600cbd8fef489e336b
Francis Acquah <cylic24darers@gmail.com>
2023-11-15 09:50:33 -0800

    our_printf.c

--- our_printf.c
+++ our_printf.c
@@ -1,22 +1,31 @@
 #include "main.h"
 /**
- * _printf - a function that produces output
+ *string_print - a function that produces output
+ *_printf - function to print strings
+ *@str: character string
  *according to a format
- *@format: character string
  *Return: -1 as fail and 0 as success
  */
 
+
+int string_print(char *str)
+{
+	int len_str = 0;
+
+	while (str[len_str] != '\0')
+		len_str++;
+	write(1, str, len_str);
+	return (len_str);
+}
+
 int _printf(const char *format, ...)
 {
+	char c, *str;
 	int counter = 0;
-	char c;
-	char *str;
-	int len_str;
 	va_list arg_list;
 
 	if (format == NULL)
 		return (-1);
-
 	va_start(arg_list, format);
 	while (*format)
 	{
@@ -43,12 +52,8 @@ int _printf(const char *format, ...)
 			}
 			else if (*format == 's')
 			{
-				len_str = 0;
 				str = va_arg(arg_list, char*);
-				while (str[len_str] != '\0')
-					len_str++;
-				write(1, str, len_str);
-				counter += len_str;
+				counter += string_print(user.str);
 			}
 		}
 	format++;
