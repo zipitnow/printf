a17c20758659ae61522c65fa0b4c109c2c5972ea
Francis Acquah <cylic24darers@gmail.com>
2023-11-15 06:14:42 -0800

    our_printf.c

--- our_printf.c
+++ our_printf.c
@@ -1,57 +1,58 @@
 #include "main.h"
+/**
+ * _printf - a function that produces output
+ *according to a format
+ *@format: character string
+ *Return: -1 as fail and 0 as success
+ */
 
 int _printf(const char *format, ...)
 {
 	int counter = 0;
-
+	char c;
+	char *str;
+	int len_str;
 	va_list arg_list;
-	va_start(arg_list, format);
 
 	if (format == NULL)
 		return (-1);
 
+	va_start(arg_list, format);
 	while (*format)
 	{
 		if (*format != '%')
 		{
-			char *str1 = va_arg(arg_list, int);
-			len_str1 = 0;
-			while (str1[len_str1] != '\0')
-				len_str1++;
-			write(1, str1, len_str1);
+			write(1, format, 1);
 			counter++;
 		}
 		else
 		{
 			format++;
-
-			if (*format == '%')
+			if (*format == '\0')
+				return (-1);
+			else if (*format == '%')
 			{
 				write(1, format, 1);
 				counter++;
-				break;
 			}
-			
-			if (*format == 'c')
+			else if (*format == 'c')
 			{
-				char c = va_arg(arg_list, int);
-				write(1, c, 1);
+				c = va_arg(arg_list, int);
+				write(1, &c, 1);
 				counter++;
 			}
-
-			if (*format == 's')
+			else if (*format == 's')
 			{
-				len_str2 = 0;
-				char *str2 = va_arg(arg_list, int);
-				while (str2[len_str2] != '\0')
-					len_str2++;
-
-				write(1, str2, len_str2);
-				counter++;
+				len_str = 0;
+				str = va_arg(arg_list, char*);
+				while (str[len_str] != '\0')
+					len_str++;
+				write(1, str, len_str);
+				counter += len_str;
 			}
 		}
 	format++;
 	}
-	va_end(arg);
-	return (counter++);
+	va_end(arg_list);
+	return (counter);
 }
--- test/main.c
+++ test/main.c
@@ -12,10 +12,10 @@ int main(void)
     int len;
     int len2;
 
-     len = _printf("Let's try to printf a simple sentence.");
+     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
 
     _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-
+    printf("Length:[test %id, %i]\n", len2, len2);
+	return (0);
 }
